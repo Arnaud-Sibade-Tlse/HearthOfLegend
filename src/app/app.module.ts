d7bed178c9e16a0dc340afc33b97491b74c941ac
GuilhemSUSA <guilhem31.susa@hotmail.com>
2018-04-03 15:12:12 +0200

    routing page accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponents, AppRoutes } from './app.routing';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AccueilComponent } from './accueil/accueil.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ...AppComponents,
-    AccueilComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,15 @@
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 export const AppRoutes: any = [
 { path: "", component: LoginComponent},
+{ path: "accueil", component: AccueilComponent},
 { path: "error", component: ErrorComponent}
 ];
 
 export const AppComponents: any = [
 	LoginComponent,
-	ErrorComponent
+	ErrorComponent,
+	AccueilComponent
 ];
