5dbb96284cd91b607c7467f29172e8fd3070656b
DarkVasDehors <sibade.arnaud@gmail.com>
2018-04-03 16:52:26 +0200

    Error : lecture json

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonReader } form '.json.error';
+
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+  styleUrls: ['./error.component.css'],
+  providers:[JsonReader]
 })
 export class ErrorComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _json : JsonReader) { }
 
   ngOnInit() {
+    this._json.getInf().subscribe(message);
   }
+
 
 }
